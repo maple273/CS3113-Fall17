afab8d22074d894a0ba813f36fd1f7e00d791f2c
Mary <mq398@nyu.edu>
2017-11-26 23:22:16 -0500

    changes made to assignment 4

---
+++ Assignment 4/Assignment 4/NYUCodebase/bg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="landscape" tilewidth="16" tileheight="16" tilecount="154" columns="14">
+ <image source="../../../CS3113/assets/graphics/Platformer assets pixel (900 assets)/backgrounds.png" width="231" height="189"/>
+</tileset>
